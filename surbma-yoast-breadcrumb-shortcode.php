c0fed42b12780afbb3d65c9b7e04d3b5f551cd75
danieledesantis <danieledesantis@users.noreply.github.com>
2015-08-01 18:26:18 +0200

    Update main file

--- surbma-yoast-breadcrumb-shortcode.php
+++ surbma-yoast-breadcrumb-shortcode.php
@@ -36,8 +36,8 @@ function surbma_yoast_breadcrumb_shortcode_shortcode( $atts ) {
 	$wpseo_internallinks = get_option( 'wpseo_internallinks' );
 
 	if ( class_exists( 'WPSEO_Breadcrumbs' ) && $wpseo_internallinks['breadcrumbs-enable'] == 1 ) {
-		$output = yoast_breadcrumb( $before, $after, false ); // Store breadcrumb inside variable
-		return $output; // Return breadcrumb
+		$output = yoast_breadcrumb( $before, $after, false ); // Daniele De Santis - Store breadcrumb inside variable
+		return $output; // Daniele De Santis - Return breadcrumb
 	}
 	elseif ( class_exists( 'WPSEO_Breadcrumbs' ) && $wpseo_internallinks['breadcrumbs-enable'] != 1 ) {
 		return __( '<p>Please enable the breadcrumb option to use this shortcode!</p>', 'surbma-yoast-breadcrumb-shortcode' );
